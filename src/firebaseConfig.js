e2d0401d87940810ccd98a6bc4a1687ebc1b56fc
patrikcepr <wkidmusic@gmail.com>
2022-01-23 17:08:59 +0100

    deployed

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCrp_BnRN_aJLihnWv8_xqq3soBj0Seevc',
+  authDomain: 'oict-project.firebaseapp.com',
+  projectId: 'oict-project',
+  storageBucket: 'oict-project.appspot.com',
+  messagingSenderId: '160822486551',
+  appId: '1:160822486551:web:275ade30fff056e6b71e52',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
